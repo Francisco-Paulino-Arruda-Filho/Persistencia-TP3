708161dec0dc7865847e62eab6e44c1eebe0f560
Erick Gabriel <erickgabrielferreira@alu.ufc.br>
2025-07-22 23:22:14 -0300

    Add payrolls

--- app/models/Payroll.py
+++ app/models/Payroll.py
@@ -1,26 +1,28 @@
-from typing import Optional, TYPE_CHECKING
-from pydantic import BaseModel
-from sqlmodel import Field, Relationship
+from typing import Optional, List
 
-if TYPE_CHECKING:
-    from app.models.Employee import EmployeeBase
+from bson import ObjectId
+from pydantic import BaseModel, Field
+from app.models.PyObjectId import PyObjectId
 
 class PayrollBase(BaseModel):
-    id: Optional[str] = Field(None, alias="_id")
     employee_id: str  # N:1
     deductions: float
     discount: float
     net_salary: float
     reference_month: str  # YYYY-MM
 
-
 class PayrollCreate(PayrollBase):
     pass
 
 class PayrollOut(PayrollBase):
-    employee: Optional["EmployeeBase"] = Relationship(back_populates="payrolls")
+    id: Optional[PyObjectId] = Field(None, alias="_id")
 
     class Config:
-        orm_mode = True  # Permite que o modelo seja usado com ORMs
-        json_encoders = {str: str}  # Serializa strings como strings no JSON
-        populate_by_name = True  # Permite que o alias (_id) seja populado por id
+        json_encoders = {ObjectId: str}
+        populate_by_name = True
+
+class PayrollPaginated(BaseModel):
+    total: int
+    skip: int
+    limit: int
+    data: List[PayrollOut]
